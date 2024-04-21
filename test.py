699edb297992c7b18bf2cca0d5ecde979c2d3243
josetomassilvaz <josetomassilvaz@gmail.com>
2024-04-20 20:59:01 -0400

    refactor: :fire: remove unused files

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import requests
 import random
 import time
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 def get_data(id, scope):
     """Fetch data from the server and measure the request time."""
@@ -22,7 +24,7 @@ def get_data(id, scope):
         return None
     
 def generate_random_id():
-    return random.randint(1, 1000000)
+    return random.randint(1, 100_000)
 
 def print_api_options():
     print("API Options:")
@@ -35,14 +37,17 @@ def main():
     cache_hits_times = []
     cache_misses_times = []
     responses_by_id = {}
+    response_source = {}
     api_options = ['classic', 'partitioned', 'replicated']
     print_api_options()
     scope = input("Enter API option: ")
 
-    for _ in range(1000):
+    for _ in range(10000):
         id = generate_random_id()
         data = get_data(id, api_options[int(scope) - 1])
         if data:
+            source = data['data']['source']
+            response_source[source] = response_source.get(source, 0) + 1
             responses_by_id[id] = data
             if data['hit']:
                 cache_hits_times.append(data['time'])
@@ -53,24 +58,37 @@ def main():
     print(f"Cache misses: {len(cache_misses_times)}")
     print(f"Cache hit average time: {sum(cache_hits_times) / len(cache_hits_times) if len(cache_hits_times) > 0 else 0:.2f} ms")
     print(f"Cache misses average time: {sum(cache_misses_times) / len(cache_misses_times) if len(cache_misses_times) > 0 else 0:.2f} ms")
-
+    print(f"Response sources: {response_source}")
     plt.figure(figsize=(14, 7))
 
-    # Scatter plot for cache hits and misses
-    plt.subplot(1, 2, 1)
+    plt.subplot(4, 1, 1)
     plt.scatter(range(len(cache_hits_times)), cache_hits_times, color='green', alpha=0.7, label='Cache Hits')
-    plt.scatter(range(len(cache_misses_times)), cache_misses_times, color='red', alpha=0.7, label='Cache Misses')
+    plt.scatter(range(len(cache_misses_times)), cache_misses_times, color='gray', alpha=0.7, label='Cache Misses')
     plt.title('Response Times for Cache Hits and Misses')
     plt.xlabel('Sample Number')
     plt.ylabel('Response Time (ms)')
     plt.legend()
 
-    # Bar chart for comparison of hits and misses
-    plt.subplot(1, 2, 2)
-    plt.bar(['Cache Hits', 'Cache Misses'], [len(cache_hits_times), len(cache_misses_times)], color=['green', 'red'])
+    plt.subplot(4, 1, 3)
+    plt.bar(range(len(cache_hits_times)), cache_hits_times, color='green', alpha=0.7, label='Cache Hits')
+    plt.bar(range(len(cache_misses_times)), cache_misses_times, color='gray', alpha=0.7, label='Cache Misses')
+    plt.title('Response Times for Cache Hits and Misses')
+    plt.xlabel('Sample Number')
+    plt.ylabel('Response Time (ms)')
+    plt.legend()
+
+
+    plt.subplot(4, 1, 2)
+    plt.bar(['Cache Hits', 'Cache Misses'], [len(cache_hits_times), len(cache_misses_times)], color=['green', 'gray'])
     plt.title('Number of Cache Hits vs Misses')
     plt.ylabel('Count')
 
+    plt.subplot(4, 1, 4)
+    plt.bar(response_source.keys(), response_source.values(), color=['green', 'gray', 'red', 'blue', 'aqua'], alpha=0.7, label=response_source.keys())
+    plt.title('Number of Responses by Source')
+    plt.xlabel('Source')
+    plt.ylabel('Responses')
+    plt.legend()
     plt.tight_layout()
     plt.show()
 
